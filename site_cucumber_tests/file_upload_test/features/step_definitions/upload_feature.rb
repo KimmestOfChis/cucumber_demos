f96b74b744fee4d31eb50cbcd6f578d480820e15
EmjayFlight <memjay3279@gmail.com>
2016-05-22 17:11:10 -0400

    Step definitions fixed

--- site_cucumber_tests/file_upload_test/features/step_definitions/upload_feature.rb
+++ site_cucumber_tests/file_upload_test/features/step_definitions/upload_feature.rb
@@ -1,18 +1,31 @@
 require 'capybara/cucumber'
 
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+
 Capybara.default_driver = :selenium
 
+capyDrive= Capybara.current_session.driver.browser
 window = Capybara.current_session.driver.browser.manage.window
+textCheck = Capybara.current_session.driver.browser
+
+wait = Selenium::WebDriver::Wait.new ignore: Selenium::WebDriver::Error::NoAlertPresentError
 
 Given(/^that I'm on Dan's Site$/) do
   visit('https://still-everglades-45219.herokuapp.com/')
   window.resize_to(1280, 720)
 end
 
-When(/^I upload a file$/) do
-  attach_file("the-file" , File.expand_path('/Users/matthewjohnson/Documents/projects/quote_machine/random_tester.rb'))
+When(/^I select a file$/) do
+  attach_file("the-file" , File.expand_path('/Users/matthewjohnson/Documents/Projects/404projects/urlFiles.txt'))
+end
+
+When(/^click "([^"]*)"$/) do |arg1|
+  click_button('Upload')
 end
 
 Then(/^I should get a file size returned$/) do
-click_button('Upload')
+	wait.until { page.driver.browser.switch_to.alert }
+	expect(textCheck.switch_to.alert.text[0,9]).to eq "File size"
 end
